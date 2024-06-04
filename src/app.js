d962a3b0d3b76c41f8951198939a07519da01d56
Raul Espinoza M <raul.espmol@gmail.com>
2024-06-04 19:42:12 -0400

    instalar cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const routes = require('./routes/index')
-
+const cors = require('cors')
 const app = express()
 
 //midlewares
 app.use(express.json())
+app.use(cors())
 
 //routes
 app.use('/', routes)
