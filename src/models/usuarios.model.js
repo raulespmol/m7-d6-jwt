05ae89fb452d9b10fc1004bc13160372dbbe6160
Raul Espinoza M <raul.espmol@gmail.com>
2024-06-04 20:02:58 -0400

    hash password

--- src/models/usuarios.model.js
+++ src/models/usuarios.model.js
@@ -1,8 +1,11 @@
 const database = require('../database/config')
+const bcrypt = require('bcryptjs')
 
 const registrarUsuario = async ({email, password, rol, lenguage}) => {
+  const passwordEncriptada = bcrypt.hashSync(password)
+
   const consulta = "INSERT INTO usuarios VALUES (DEFAULT, $1, $2, $3, $4) RETURNING *"
-  const values = [email, password, rol, lenguage]
+  const values = [email, passwordEncriptada, rol, lenguage]
 
   const {rows: usuario} = await database.query(consulta, values)
   return usuario
