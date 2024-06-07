a3f5c6e76d2b9aaa0accc047f974680c15b86326
Raul Espinoza M <raul.espmol@gmail.com>
2024-06-06 21:17:31 -0400

    logs middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes/index')
 const cors = require('cors')
+const { handleRequestLogs } = require('./middlewares/logs.middleware')
 const app = express()
 
 //midlewares
@@ -8,6 +9,6 @@ app.use(express.json())
 app.use(cors())
 
 //routes
-app.use('/', routes)
+app.use('/', handleRequestLogs, routes)
 
 module.exports = app
--- src/middlewares/logs.middleware.js
+++ src/middlewares/logs.middleware.js
@@ -2,16 +2,12 @@ const moment = require('moment')
 
 const handleRequestLogs = (req, res, next) => {
   const time = moment().format('DD/MM/YY HH:mm:ss')
-  const {method, query, params, originalUrl} = req
+  const {method, originalUrl} = req
   const path = originalUrl.split('?')[0]
   
   const message = `
   *****
   [${time}] Se ha realizado una consulta '${method} ${path}'
-  Queries: 
-  ${JSON.stringify(query, null, 2)}
-  Params: 
-  ${JSON.stringify(params, null, 2)}
   *****
   `
   
